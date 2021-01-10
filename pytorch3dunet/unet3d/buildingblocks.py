a9ad5e920e0a5f4b6ad0c14529c1a05ca90ee300
Yinan Zhang <yinancy02@gmail.com>
2021-01-09 20:11:57 -0800

    add rational

--- pytorch3dunet/unet3d/buildingblocks.py
+++ pytorch3dunet/unet3d/buildingblocks.py
@@ -5,6 +5,35 @@ from torch import nn as nn
 from torch.nn import functional as F
 
 
+class Rational(torch.nn.Module):
+    """Rational Activation function.
+    Implementation provided by Mario Casado (https://github.com/Lezcano)
+    It follows:
+    `f(x) = P(x) / Q(x),
+    where the coefficients of P and Q are initialized to the best rational 
+    approximation of degree (3,2) to the ReLU function
+    # Reference
+        - [Rational neural networks](https://arxiv.org/abs/2004.01902)
+    """
+    def __init__(self):
+        super().__init__()
+        self.coeffs = torch.nn.Parameter(torch.Tensor(4, 2))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.coeffs.data = torch.Tensor([[1.1915, 0.0],
+                                         [1.5957, 2.383],
+                                         [0.5, 0.0],
+                                         [0.0218, 1.0]])
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        self.coeffs.data[0,1].zero_()
+        exp = torch.tensor([3., 2., 1., 0.], device=input.device, dtype=input.dtype)
+        X = torch.pow(input.unsqueeze(-1), exp)
+        PQ = X @ self.coeffs
+        output = torch.div(PQ[..., 0], PQ[..., 1])
+        return output
+
 def conv3d(in_channels, out_channels, kernel_size, bias, padding):
     return nn.Conv3d(in_channels, out_channels, kernel_size, padding=padding, bias=bias)
 
@@ -64,8 +93,10 @@ def create_conv(in_channels, out_channels, kernel_size, order, num_groups, paddi
                 modules.append(('batchnorm', nn.BatchNorm3d(in_channels)))
             else:
                 modules.append(('batchnorm', nn.BatchNorm3d(out_channels)))
+        elif char == 'a':
+            modules.append(('Rational', Rational()))
         else:
-            raise ValueError(f"Unsupported layer type '{char}'. MUST be one of ['b', 'g', 'r', 'l', 'e', 'c']")
+            raise ValueError(f"Unsupported layer type '{char}'. MUST be one of ['b', 'g', 'r', 'l', 'e', 'c', 'a']")
 
     return modules
 
